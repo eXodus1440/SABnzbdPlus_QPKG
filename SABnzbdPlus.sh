5a213515ec36d0ace25946c894f29ec5713ee81e
Steve Bauer <stev8541@mm7155fd56.home>
2015-05-31 22:16:32 +0100

    Updated init script

--- SABnzbdPlus.sh
+++ SABnzbdPlus.sh
@@ -2,7 +2,6 @@
 CONF=/etc/config/qpkg.conf
 QPKG_NAME="SABnzbdPlus"
 QPKG_DIR=`$CMD_GETCFG $QPKG_NAME Install_Path -f ${CONF}`
-#PYTHON_PATH=`$CMD_GETCFG python Install_Path -f ${CONF}`/bin/python
 PUBLIC_SHARE=`$CMD_GETCFG SHARE_DEF defPublic -d Public -f /etc/config/def_share.info`
 WEBUI_IP=`$CMD_GETCFG misc host -f ${QPKG_DIR}/.sabnzbd/sabnzbd.ini`
 API_KEY=`$CMD_GETCFG misc api_key -f ${QPKG_DIR}/.sabnzbd/sabnzbd.ini`
@@ -38,7 +37,7 @@ if [ -z $BASE ]; then
 fi
 if [ -z $BASE ] ; then
   echo "The Public share not found."
-  $CMD_WLOG "[SABnzbdPlus] The Public share not found." 1
+  $CMD_WLOG "[$QPKG_NAME] The Public share not found." 1
   exit 1
 fi
 
@@ -53,20 +52,19 @@ case "$1" in
     fi
 
     if [ -f ${QPKG_DIR}/sabnzbd-${WEBUI_PORT}.pid ]; then
-      echo "SABnzbdPlus is currently running or hasn't been shutdown properly. Please stop it before starting a new instance."
-      #echo "If not so, then delete the file named ${QPKG_DIR}/sabnzbd-${WEBUI_PORT}.pid and try again."
+      echo "$QPKG_NAME is currently running or hasn't been shutdown properly. Please stop it before starting a new instance."
       exit 0
     fi
-
-    # create the lock file
-    #touch ${QPKG_DIR}/lock
   
     echo "Creating Library links ..."
     [ -d /root/.sabnzbd ] || $CMD_LN -sf ${QPKG_DIR}/.sabnzbd /root/.sabnzbd
     [ -d /root/Downloads ] || $CMD_LN -sf ${BASE}/${PUBLIC_SHARE}/Downloads /root/Downloads
     [ -d /root/nzb ] || $CMD_LN -sf ${BASE}/${PUBLIC_SHARE}/nzb /root/nzb
+    [ -f /usr/bin/ionice ] || $CMD_RM -f /usr/bin/ionice
+    [ -f /opt/lib/python2.6/site-packages/yenc.py ] || $CMD_RM -f /opt/lib/python2.6/site-packages/yenc.py
+    [ -f /opt/lib/python2.6/site-packages/_yenc.so ] || $CMD_RM -f /opt/lib/python2.6/site-packages/_yenc.so
 
-    echo "Starting SABnzbdPlus ..."
+    echo "Starting $QPKG_NAME ..."
     if [ -f /root/.sabnzbd/sabnzbd.ini ]; then
       /opt/bin/python2.6 ${QPKG_DIR}/SABnzbd.py --pid ${QPKG_DIR} -f /root/.sabnzbd/sabnzbd.ini -d &
     else
@@ -76,55 +74,55 @@ case "$1" in
     ;;
 
   stop)
-    if [ ! -f "$QPKG_DIR/sabnzbd-$WEBUI_PORT.pid" ]; then
-      echo "SABnzbdPlus hasn't been enabled or started ..."
-      # Clean up symlinks in event SABnzbdPlus was likely shutdown outside QPKG manager
-      if [ -d /root/.sabnzbd ]; then $CMD_RM -rf /root/.sabnzbd ; fi
-      if [ -d /root/Downloads ]; then $CMD_RM -rf /root/Downloads ; fi
-      if [ -d /root/nzb ]; then $CMD_RM -rf /root/nzb ; fi
-      exit 0
-    
-    else 
-      echo "Stopping SABnzbdPlus ..."
+    if [ -f "$QPKG_DIR/sabnzbd-$WEBUI_PORT.pid" ]; then
       PID=$(cat ${QPKG_DIR}/sabnzbd-${WEBUI_PORT}.pid)
-      if [ -n ${WEBUI_PASS} ]; then
-        /usr/bin/wget -q --delete-after "http://${WEBUI_IP}:${WEBUI_PORT}/sabnzbd/api?mode=shutdown&apikey=${API_KEY}" &
+      if [ `ps ax | grep -v grep | grep -c ${PID}` = '0' ]; then
+        echo "$QPKG_NAME not running, cleaning up ${QPKG_DIR}/sabnzbd-${WEBUI_PORT}.pid ..."
+        $CMD_RM -f ${QPKG_DIR}/sabnzbd-$WEBUI_PORT.pid
+        exit 1
       else
-        /usr/bin/wget -q --delete-after "http://${WEBUI_IP}:${WEBUI_PORT}/sabnzbd/api?mode=shutdown&ma_username=${WEBUI_USER}&ma_password=${WEBUI_PASS}&apikey=${API_KEY}" &
-      fi
-
-      # Waiting for SABnzbdPlus to shutdown gracefully
-      if [ -n ${PID} ]; then
-        KWAIT=${SHUTDOWN_WAIT}
-        COUNT=0
-        until [ `ps ax | grep -v grep | grep -c ${PID}` = '0' ] || [ "${COUNT}" -gt "${KWAIT}" ]
-        do
-          echo "Waiting ${COUNT}/${SHUTDOWN_WAIT} seconds for ${QPKG_NAME} processes to exit ..."
-          sleep 1
-          COUNT=$(( $COUNT + 1 ))
-        done
-      
-        # Killing SABnzbdPlus and par2 after SHUTDOWN_WAIT period
-        if [ "${COUNT}" -gt "${KWAIT}" ]; then
-          echo "Killing ${QPKG_NAME} processes which didn't stop after ${SHUTDOWN_WAIT} seconds"
-          kill -9 `ps ax | grep 'SABnzbd' | grep -v grep | awk ' { print $1;}'`
-          kill -9 `ps ax | grep 'par2' | grep -v grep | awk ' { print $1;}'`
+        echo "Stopping $QPKG_NAME ..."
+        if [ -n ${WEBUI_PASS} ]; then
+          $CMD_WGET -q --delete-after "http://${WEBUI_IP}:${WEBUI_PORT}/sabnzbd/api?mode=shutdown&apikey=${API_KEY}" &
+        else
+          $CMD_WGET -q --delete-after "http://${WEBUI_IP}:${WEBUI_PORT}/sabnzbd/api?mode=shutdown&ma_username=${WEBUI_USER}&ma_password=${WEBUI_PASS}&apikey=${API_KEY}" &
         fi
-      fi
-      # Cleaning up other items
-      sleep 2
-      $CMD_RM -f ${QPKG_DIR}/sabnzbd-$WEBUI_PORT.pid
-      echo "Removing Library links ..."
-      if [ -d /root/.sabnzbd ]; then $CMD_RM -rf /root/.sabnzbd ; fi
-      if [ -d /root/Downloads ]; then $CMD_RM -rf /root/Downloads ; fi
-      if [ -d /root/nzb ]; then $CMD_RM -rf /root/nzb ; fi
-      if [ -f /usr/bin/ionice ]; then $CMD_RM -f /usr/bin/ionice ; fi
-      if [ -f /opt/lib/python2.6/site-packages/yenc.py ]; then $CMD_RM -f /opt/lib/python2.6/site-packages/yenc.py ; fi
-      if [ -f /opt/lib/python2.6/site-packages/_yenc.so ]; then $CMD_RM -f /opt/lib/python2.6/site-packages/_yenc.so ; fi
 
-      # Disabling SABnzbdPlus within qpkg.conf
-      /sbin/setcfg $QPKG_NAME Enable FALSE -f $CONF
-    fi    
+        # Waiting for SABnzbdPlus to shutdown gracefully
+        if [ -n ${PID} ]; then
+          KWAIT=${SHUTDOWN_WAIT}
+          COUNT=0
+          until [ `ps ax | grep -v grep | grep -c ${PID}` = '0' ] || [ "${COUNT}" -gt "${KWAIT}" ]
+          do
+            echo "Waiting ${COUNT}/${SHUTDOWN_WAIT} seconds for ${QPKG_NAME} processes to exit ..."
+            sleep 1
+            COUNT=$(( $COUNT + 1 ))
+          done
+        
+          # Killing SABnzbdPlus and par2 after SHUTDOWN_WAIT period
+          if [ "${COUNT}" -gt "${KWAIT}" ]; then
+            echo "Killing ${QPKG_NAME} processes which didn't stop after ${SHUTDOWN_WAIT} seconds"
+            kill -9 `ps ax | grep 'SABnzbd' | grep -v grep | awk ' { print $1;}'`
+            kill -9 `ps ax | grep 'par2' | grep -v grep | awk ' { print $1;}'`
+          fi
+        fi
+    fi
+
+    # Cleaning up other items
+    $CMD_SLEEP 2
+    
+    # Clean up symlinks in event that SABnzbdPlus was shutdown outside QPKG manager
+    echo "Removing Library links ..."
+    if [ -d /root/.sabnzbd ]; then $CMD_RM -rf /root/.sabnzbd ; fi
+    if [ -d /root/Downloads ]; then $CMD_RM -rf /root/Downloads ; fi
+    if [ -d /root/nzb ]; then $CMD_RM -rf /root/nzb ; fi
+    if [ -f /usr/bin/ionice ]; then $CMD_RM -f /usr/bin/ionice ; fi
+    if [ -f /opt/lib/python2.6/site-packages/yenc.py ]; then $CMD_RM -f /opt/lib/python2.6/site-packages/yenc.py ; fi
+    if [ -f /opt/lib/python2.6/site-packages/_yenc.so ]; then $CMD_RM -f /opt/lib/python2.6/site-packages/_yenc.so ; fi
+
+    # Disabling SABnzbdPlus within qpkg.conf
+    $CMD_SETCFG $QPKG_NAME Enable FALSE -f $CONF
+    exit 0    
     ;;
 
   restart)
